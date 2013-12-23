e70c1515b541455a9048139be2f259ab17ac6340
AppWerft <rs@hamburger-appwerft.de>
2013-12-23 09:35:25 +0100

    refactoring; splitting in modules

--- Resources/app.js
+++ Resources/app.js
@@ -2,4 +2,6 @@
 	Ti.App.TinyBrainProxy = require('model/tinybrain');
 	require('ui/main.window').create();
 })();
+
+// /Users/rainerschleevoigt/android-sdk/platform-tools/adb -d logcat | grep TiAPI
    
